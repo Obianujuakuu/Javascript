c4f657455bc9c281d0a1afcc5acf0ef27692645f
Obianujuakuu <99666909+Obianujuakuu@users.noreply.github.com>
2023-01-14 15:03:26 +0000

    updated index.html,added css file and refactored script.js

--- js/script.js
+++ js/script.js
@@ -1,64 +1,43 @@
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissors = document.querySelector('#scissors')
 
 
+let choices = ['rock', 'paper', 'scissors']
 
-let playerScore = 0;
-let compScore = 0;
-
+//function to get random num for computer
 function getComputerChoice(){
-    let randomNum = Math.floor(Math.random() * 3);
-    if(randomNum === 0){
-        return "Rock"
-    }
-    if(randomNum === 1){
-        return "Paper"
-    }
-    if(randomNum === 2){
-        return "Scissors"
-    }
-}
+    let randomNum = choices[Math.floor(Math.random() * choices.length)];
+  console.log(randomNum)
+};
 
-
+// Function to play a round of game
 function playRound(playerSelection, computerSelection){
-    console.log("1", playerSelection, "2", computerSelection);
-    if(playerSelection === computerSelection)
-        console.log("It's a Tie!")
-        if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "PAPER") {
-            compScore++; 
-            return "You Lose! Paper Beat Rock"
-        } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS") {
-            playerScore++;
-            return "You Win! Rock Beat Sissors"
-        } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK") {
-            playerScore++
-            return "You Win! Paper Beat Rock"
-        } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSORS") {
-            compScore++
-            return "You Lose! Scissors Beat Paper" 
-        } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "ROCK") {
-            compScore++
-            return "You Lose! Rock Beat Sissors"
-        } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER") {
-            playerScore++
-            return "You Win! Scissors Beat Paper" 
+    if (playerSelection === computerSelection){
+        result = "It's A Tie";
+    
+    }
+    if (
+        (playerSelection === "rock" && computerSelection === "paper")
+        ||(playerSelection === "paper" && computerSelection === "scissors")
+        ||(playerSelection === " scissors" && computerSelection === "rock")
+        )
+        {
+            computerScore++;
+            result = "You Lost!";
+            
         }
+        if(
+            (playerSelection === "rock" && computerSelection === "scissors")
+            ||(playerSelection === "paper" && computerSelection === "rock")
+            ||(playerSelection === "scissors" && computerSelection === "paper")
+            ){
+                playerScore++;
+                result = "You Win!";
+                
+            }
+        
 }
 
 
 
-function game(){
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Make a choice").toLowerCase()
-        const computerSelection = getComputerChoice()
-        console.log(playRound(playerSelection, computerSelection))
-     }
-
-     if (playerScore > compScore){
-        return "You Beat The Computer! Congratulations"
-     } else if (playerScore < compScore) {
-        return "You Lost To The Computer! Practice More"
-     } else {
-        return "It,s A Game Tie! Not Bad At All"
-     }
-}
- 
-console.log(game())
